d2ec874a8da6709a710d9924721728d1483840eb
Dimitri Mockelyn <dimitri.mockelyn@gmail.com>
2017-10-04 22:19:27 +0200

    changement des styles du menu de gauche

--- ui/app/views/header/footer.jsx
+++ ui/app/views/header/footer.jsx
@@ -4,20 +4,10 @@ import {component as Popin} from 'focus-components/application/popin';
 import MentionsLegales from './mentions-legales';
 export default React.createClass({
     displayName: 'Footer',
-    togglePopinMention() {
-        this.refs.mention.toggleOpen();
-    },
     render() {
-        const ver = getConfig().VERSION;
         return (
             <div data-focus="footer">
-                <div data-focus="scrolling-popin">
-                <Popin ref='mention'>
-                    <MentionsLegales/>
-                </Popin>
-                </div>
-                <div data-focus="mention-legales" onClick={this.togglePopinMention}>Mention legales</div>
-                <div>v {ver} <div data-focus="logo"/></div>
+                
             </div>
         );
     }
