5d63edfee315c65c5c28e2f4440d570404407696
Dimitri Mockelyn <dimitri.mockelyn@gmail.com>
2017-12-11 22:08:43 +0100

    Changement de l'ordre du menu

--- ui/app/views/menu/menu-left.jsx
+++ ui/app/views/menu/menu-left.jsx
@@ -9,15 +9,15 @@ function computeMenuItem() {
     return [
         { icon: 'home', name: 'Accueil', route: '' },
         { icon: 'date_range', name: 'Agenda', route: 'agenda'},
-        { icon: 'business', name: 'Partenaires', route: 'partenaires'},
-        { icon: 'call', name: 'Contacts et informations', route: 'contact'}
+        { icon: 'call', name: 'Contacts et informations', route: 'contact'},
+        { icon: 'business', name: 'Partenaires', route: 'partenaires'}
     ];
 }
 
 export default () => {
     let menu = computeMenuItem();
     if (userHelper.getLogin()) {
-        menu.push({icon: 'history', name: 'Historique', route: 'historique'})
+        menu.splice(2, 0, {icon: 'history', name: 'Historique', route: 'historique'});
     }
     if (userHelper.getLogin() && userHelper.getLogin().isAdmin) {
         menu.push({icon: 'show_chart', name: 'Administration', route: 'admin'})
