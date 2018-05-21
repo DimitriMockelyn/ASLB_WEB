05dc456443708cb745da4ed47e02d285259f577d
Dimitri Mockelyn <dimitri.mockelyn@gmail.com>
2018-05-21 13:18:57 +0100

    wording

--- ui/app/views/inscription/index.jsx
+++ ui/app/views/inscription/index.jsx
@@ -23,16 +23,25 @@ export default React.createClass({
                 </Panel>
                 <Panel title='Nous rejoindre'>
                     <div data-focus='column'>
-                        <label>Un dossier d’inscription doit être retiré au bureau ou téléchargé et doit être rendu dûment rempli et signé.</label>
-                        <label>Il comporte les documents ci-dessous :</label>
+                        <label>Téléchargez <div className='link boldbig' onClick={() => {window.location.href = './Inscription_ASLB.zip'}}>içi</div> votre dossier d'inscription</label>
+                        <label>La première étape avant toute inscription est de s’assurer que vous disposez bien d’un certificat médical. Prenez rendez-vous chez votre médecin si vous n’en avez pas.</label>
+                        <label>Le dossier d’inscription comporte les documents ci-dessous :</label>
                         <ul>
                             <li><label>Fiche d'adhésion (à compléter et signer)</label></li>
                             <li><label>Certificat médical (à faire remplir par son médecin traitant - valable 3 ans)</label></li>
                             <li><label>Décharge de responsabilité (à compléter et signer)</label></li>
-                            <li><label>Règlement intérieur (à signer)</label></li>
+                            <li><label>Règlement intérieur (à parapher sur chaque page et à signer sur la dernière page)</label></li>
                         </ul>
-                        <label>Vous pouvez télécharger votre dossier d'inscription <div className='link boldbig' onClick={() => {window.location.href = './Inscription_ASLB.zip'}}>içi</div> </label>
-                        <label>Pour les paiement par virement ou PayPal, veuillez a bien lire la fiche d'adhésion.</label>
+                        
+                        <label>
+                            Vous pouvez déposer votre dossier d’inscription complété dans la boîte aux lettres de l’ASLB (n°101). Elle se trouve dans la salle La Poste (demander sa localisation à l’accueil)
+                        </label>
+                        <label>
+                                La cotisation annuelle est de 30 € et peut être payée en liquide, par chèque (à l’ordre de l’ASLB), par virement (IBAN fourni dans la fiche d’adhésion) ou par Paypal (cf fiche d’adhésion). . 
+                        </label>
+                        <label>
+                            Les paiements par virement ou PayPal sont vivement encouragés, car ils n’engendrent pas de manutention. 
+                        </label>
                     </div>
                 </Panel>
             </ScrollspyContainer>
